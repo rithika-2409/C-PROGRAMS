1f2dc92d7733cb717b5ed163d9340d78308dd33c
rithika-2409 <rithikanatarajan24@gmail.com>
2026-09-18 19:27:54 +0530

    Add files via upload

---
+++ EXP-4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+// Delegate declaration
+public delegate void Notify();
+class Publisher
+{
+    // Event declaration
+    public event Notify OnMessage;
+    public void SendMessage()
+    {
+        Console.WriteLine("Publisher: Sending Message...");
+        if (OnMessage != null)
+            OnMessage();
+    }
+}
+class Subscriber
+{
+    public void ReceiveMessage()
+    {
+        Console.WriteLine("Subscriber: Message Received!");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Publisher p = new Publisher();
+        Subscriber s = new Subscriber();
+
+        // Subscribe to the event
+        p.OnMessage += s.ReceiveMessage;
+        // Raise the event
+        p.SendMessage();
+        Console.ReadKey();
+    }
+}
+
