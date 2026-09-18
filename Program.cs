d9fffad8ba52c03dbd973a74039b00cb4631f127
rithika-2409 <rithikanatarajan24@gmail.com>
2026-09-18 19:17:40 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+class Student
+{
+    string Name;
+    int RollNo;
+    string Department;
+    public void GetDetails()
+    {
+        Name = "Rithika";
+        RollNo = 101;
+        Department = "Information Technology";
+    }
+    public void DisplayDetails()
+    {
+        Console.WriteLine("Student Details");
+        Console.WriteLine("---------------------");
+        Console.WriteLine("Name       : " + Name);
+        Console.WriteLine("Roll No    : " + RollNo);
+        Console.WriteLine("Department : " + Department);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student();
+        s1.GetDetails();
+        s1.DisplayDetails();
+
+        Console.ReadLine();
+    }
+}
+
+
+
