c4707f065d56d0dc5dc7075ccd2366622ed1e5d6
rithika-2409 <rithikanatarajan24@gmail.com>
2026-09-18 19:23:34 +0530

    Add files via upload

---
+++ EXP-2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Person
+{
+    protected string name;
+    protected int age;
+    public void GetPersonDetails()
+    {
+        Console.Write("Enter Name: ");
+        name = Console.ReadLine();
+        Console.Write("Enter Age: ");
+        age = Convert.ToInt32(Console.ReadLine());
+    }
+}
+class Student : Person
+{
+    private int rollNo;
+    public void GetStudentDetails()
+    {
+        GetPersonDetails();
+        Console.Write("Enter Roll Number: ");
+        rollNo = Convert.ToInt32(Console.ReadLine());
+    }
+    public void DisplayDetails()
+    {
+        Console.WriteLine("\n--- Student Details ---");
+        Console.WriteLine("Name        : " + name);
+        Console.WriteLine("Age         : " + age);
+        Console.WriteLine("Roll Number : " + rollNo);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s = new Student();
+        s.GetStudentDetails();
+        s.DisplayDetails();
+        Console.ReadKey();
+    }
+}
+
